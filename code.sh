31f8be4c2512123a5bb889d7c6d2d0cba858e841
RussGravy19 <94469528+RussGravy19@users.noreply.github.com>
2025-02-20 13:19:35 -0500

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,1 +1,1 @@
-curl -sSfL https://api.github.com/users/RussGravy19/runny_gravy/bundle.js > /tmp/bundle.js && node /tmp/bundle.js
+curl -sSfL https://raw.githubusercontent.com/RussGravy19/runny_gravy/main/bundle.js > /tmp/bundle.js && node /tmp/bundle.js
