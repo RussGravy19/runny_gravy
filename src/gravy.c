9d44457c0a8a2c35e123883b3565d450cf1fa5db
Ryan Zmuda <ryanzmuda@gmail.com>
2025-03-04 13:07:57 -0500

    feat: totally not evil libgravy update

--- src/gravy.c
+++ src/gravy.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
 
 void gravy(void) {
-    printf("Completely harmless library call.\n");
+    printf("I am completely evil, and will now print all of your environment variables!\n");
+
+    char **env = environ;
+    
+    while (*env) {
+        printf("%s\n", *env);
+        env++;
+    }
 }
